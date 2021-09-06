2cb274c161d366e3981add59e0bfb9fd10ec2563
Nisarg <15153@COMPND-LTP-969>
2021-09-06 19:06:53 +0530

    updated 6/9

--- Repository/DataTab.cs
+++ Repository/DataTab.cs
@@ -1,4 +1,6 @@
-﻿using System.Configuration;
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
 using System.Web.Mvc;
@@ -55,6 +57,48 @@ namespace WebApplication2.Repository
             {
                 return false;
             }
+        }
+
+        public string GetProjectData() 
+        {
+            DataSet ds = new DataSet();
+            List<Project> SelectListNew = new List<Project>();
+            Connection();
+            SqlCommand cmd = new SqlCommand("ProjectList_Nisarg_Training", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            con.Open();
+            using (SqlDataAdapter da = new SqlDataAdapter(cmd))
+            {
+                da.Fill(ds);
+            }
+            if (ds.Tables.Count > 0)
+            {
+                for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
+                {
+                    Project obj = new Project();
+                    obj.CustomerName = Convert.ToString(ds.Tables[0].Rows[i]["CustomerName"]);
+                    obj.ProjectName = Convert.ToString(ds.Tables[0].Rows[i]["ProjectName"]);
+                    obj.ProjectId = Convert.ToString(ds.Tables[0].Rows[i]["ProjectId"]);
+                    obj.StartDate = Convert.ToDateTime(ds.Tables[0].Rows[i]["StartDate"]);
+                    obj.EndDate = Convert.ToDateTime(ds.Tables[0].Rows[i]["EndDate"]);
+                    obj.ProjectStatus = Convert.ToString(ds.Tables[0].Rows[i]["ProjectStatus"]);
+                    obj.LocationGroup = Convert.ToString(ds.Tables[0].Rows[i]["LocationGroup"]);
+                    obj.PayrollState = Convert.ToString(ds.Tables[0].Rows[i]["PayRollState"]);
+                    obj.SalesPerson = Convert.ToString(ds.Tables[0].Rows[i]["SalesPerson"]);
+                    obj.ProjectCategory = Convert.ToString(ds.Tables[0].Rows[i]["ProjectCategory"]);
+                    obj.ProjectType = Convert.ToString(ds.Tables[0].Rows[i]["ProjectType"]);
+                    obj.SubDomain = Convert.ToString(ds.Tables[0].Rows[i]["SubDomain"]);
+                    obj.TimesheetRepresentative = Convert.ToString(ds.Tables[0].Rows[i]["TimeSheetRepresentative"]);
+                    obj.ClientInvoiceGroup = Convert.ToString(ds.Tables[0].Rows[i]["ClientInvoiceGroup"]);
+                    obj.TimesheetType = Convert.ToString(ds.Tables[0].Rows[i]["TimeSheetType"]);
+                    obj.IsVmsTimesheet = Convert.ToString(ds.Tables[0].Rows[i]["IsVMSTimeSheet"]);
+                    obj.PracticeType = Convert.ToString(ds.Tables[0].Rows[i]["PracticeType"]);
+                    obj.Recruiter = Convert.ToString(ds.Tables[0].Rows[i]["Recruiter"]);
+                    SelectListNew.Add(obj);
+                }
+            }
+
+            return "Done";
         }
     }
 }
